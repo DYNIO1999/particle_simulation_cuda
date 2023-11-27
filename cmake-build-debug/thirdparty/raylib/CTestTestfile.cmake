ee573e86a3304487b7227d5ce24da090d4e28a45
Daniel Skrobot <32042520+DYNIO1999@users.noreply.github.com>
2023-11-27 10:01:55 +0100

    Delete cmake-build-debug directory

--- cmake-build-debug/thirdparty/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dynio/Development/Dev_Uni/particle_simulation_cuda/thirdparty/raylib
-# Build directory: /home/dynio/Development/Dev_Uni/particle_simulation_cuda/cmake-build-debug/thirdparty/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
